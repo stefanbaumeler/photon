efce1065f2cb3d9e53a5723f7ce6bb988847b651
sbaumeler <stefan.baumeler@hotmail.com>
2022-09-23 12:48:27 +0200

    Sass => SCSS

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -17,7 +17,7 @@ import 'react-datepicker/dist/react-datepicker.css'
 //     plugins: [followCursor]
 // })
 
-import styles from '../styles/index.sass'
+import styles from '../styles/index.scss'
 
 styles.length
 
