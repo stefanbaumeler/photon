12c4532d90328fa74063e66ef34d4320b9a21a59
sbaumeler <stefan.baumeler@hotmail.com>
2022-10-28 13:00:10 +0100

    ci/cd

--- scripts/seed-db.js
+++ scripts/seed-db.js
@@ -5,4 +5,6 @@ dotenv.config({
     path: process.env.NODE_ENV ? `./api/.env.${process.env.NODE_ENV}` : './api/.env'
 })
 
-exec('yarn workspace @photon/api knex seed:run')
+exec('yarn workspace @photon/api knex seed:run', (a, b, c) => {
+    console.log(a, b, c)
+})
