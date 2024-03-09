a7f0395276c2e1eb69df85455e4f09dc5da23ae8
sbaumeler <stefan.baumeler@hotmail.com>
2024-03-09 20:35:32 +0100

    Reset nav on logout

--- web/src/components/controls/SignOutControl.tsx
+++ web/src/components/controls/SignOutControl.tsx
@@ -4,6 +4,8 @@ import { ETrans } from '@/types/translations'
 import { useTranslation } from 'react-i18next'
 import { useMSignOut } from '@photon/schema'
 import { useRouter } from 'next/router'
+import { useNavContext } from '@/providers'
+import { ENavs } from '@/types/app'
 
 type Props = {
     dropdown?: boolean
@@ -16,10 +18,12 @@ export const SignOutControl = ({
     const { t } = useTranslation()
     const [, out] = useMSignOut()
     const router = useRouter()
+    const { setActive: setActiveNavigation } = useNavContext()
 
     const action = () => {
         out({}).then(() => {
             window.localStorage.removeItem('photon')
+            setActiveNavigation([ENavs.HOME])
             router.push('/login')
         })
         callback && callback()
